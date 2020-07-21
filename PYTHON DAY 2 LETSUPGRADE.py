8c8d7916b27bc73d290958630218046a97f63330
PanitaVaishnavi <68539219+PanitaVaishnavi@users.noreply.github.com>
2020-07-21 15:07:10 +0530

    Add files via upload

---
+++ PYTHON DAY 2 LETSUPGRADE.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[2]:
+
+
+print("Hello World")
+
+
+# In[4]:
+
+
+print("Hello       python")
+
+
+# In[5]:
+
+
+print(''' HELLO EVERY ONE''')
+
+
+# In[6]:
+
+
+print("python's world")
+
+
+# In[7]:
+
+
+print("Hello\nPython")
+
+
+# In[8]:
+
+
+print("Hello\tPython")
+
+
+# In[9]:
+
+
+print("Hello\aPython")
+
+
+# In[13]:
+
+
+name="panita"
+marks=98.67
+age=18
+print("The name is",name,"marks is",marks,"age is",age)
+
+
+# In[14]:
+
+
+_a=10
+print(_a) 
+
+
+# In[15]:
+
+
+x=y=10
+print(id(x))
+print(id(y))
+
+
+# In[16]:
+
+
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+
+# In[17]:
+
+
+a=10
+b=20
+a==b
+
+
+# In[18]:
+
+
+c=10
+a==c
+
+
+# In[19]:
+
+
+a<b and b<c
+
+
+# In[20]:
+
+
+a>c or b<c
+
+
+# In[21]:
+
+
+a=240
+bin(a)
+
+
+# In[22]:
+
+
+not a<b
+
+
+# In[23]:
+
+
+not c==a
+
+
+# In[28]:
+
+
+stri="panita"
+"x" in string
+"a" not in string
+
+
+# In[29]:
+
+
+2/2**4+(2*5)
+
+
+# In[ ]:
+
+
+
+
